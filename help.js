41855d2e7c03b856b31dc319a1147ac72c297d2c
jairoMolina9 <molinaandres9991@gmail.com>
2020-08-21 16:55:08 -0400

    removed extra ~

--- help.js
+++ help.js
@@ -7,7 +7,7 @@ function getHelp() {
   	.setThumbnail('https://i.imgur.com/mIdF4ZW.png')
   	.addFields(
   		{ name: 'Commands',
-      value: '`!flame help` - shows this message.\n`!flame @user` - sends saved or unsaved flame with random gif.\n`!flame -s @user text` - saves a custom flame **(1)**.\n`!flame -r @user` - sends unsaved random flame.\n~~~`!flame -g @user text` - sends saved or unsaved flame with custom gif **(2)**~~~.\n~~~`!flame -r -g @user text` - sends unsaved random flame with custom gif **(2)**~~~.'
+      value: '`!flame help` - shows this message.\n`!flame @user` - sends saved or unsaved flame with random gif.\n`!flame -s @user text` - saves a custom flame **(1)**.\n`!flame -r @user` - sends unsaved random flame.\n~~`!flame -g @user text` - sends saved or unsaved flame with custom gif **(2)**~~.\n~~`!flame -r -g @user text` - sends unsaved random flame with custom gif **(2)**~~.'
       },
       { name: 'Other', value: '**(1)** - *replace text with custom flame*\n**(2)** - *replace text with custom gif filter, one keyword*' },
   	)
