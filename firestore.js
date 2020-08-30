23248ceb39f5adece1588082ce5bfbccbb242efb
Andres Molina <33586685+jairoMolina9@users.noreply.github.com>
2020-08-30 13:12:10 -0400

    Update firestore.js
    Co-authored-by: Edwin Carbajal <exaviercarbajal@gmail.com>

--- firestore.js
+++ firestore.js
@@ -1,5 +1,5 @@
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey");
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
